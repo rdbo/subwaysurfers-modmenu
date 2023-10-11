98b9f38adf7707771f64a3afe2a1ab4d12cfcb2a
rdbo <rdbodev@gmail.com>
2023-10-11 11:26:58 -0300

    added frida-java-menu

--- frida-subwaysurf-mod/src/index.ts
+++ frida-subwaysurf-mod/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world");
+import "frida-java-menu";
+import { menuIcon } from "./menu-icon.js";
+
+function main() {
+    Menu.theme = Menu.Theme.LGL;
+    let menu = new Menu.JavaMenu("Subway Surfers Mod", "modded by rdbo");
+    menu.icon(menuIcon, "Normal");
+    
+    menu.add(menu.button("Button", function () {
+        this.allCaps = true;
+        this.text = "not a button";
+        this.backgroundColor = Menu.theme.collapseColor;
+    }));
+
+    menu.show();
+}
+Menu.MainActivity.waitForInit(main);
